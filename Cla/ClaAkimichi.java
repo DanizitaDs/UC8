a1d375d192fc59b2510b907b9d2a24e92ccab3ae
DanizitaDs <162368805+DanizitaDs@users.noreply.github.com>
2024-10-30 15:06:04 -0300

    Add files via upload

---
+++ Cla/ClaAkimichi.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cla;
+
+/**
+ *
+ * @author DANIELLEYASMINDIASPA
+ */
+public class ClaAkimichi {
+     public void habilidadeEspecial(){
+    System.out.println("ampliação corporal");
+    }
+}
