b04c0c2c83b2c0183bba03de95d17feb1fb3700f
DanizitaDs <162368805+DanizitaDs@users.noreply.github.com>
2024-11-27 16:07:36 -0300

    Add files via upload

---
+++ aula2/scaner.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula2;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DANIELLEYASMINDIASPA
+ */
+public class scaner {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int numero1,numero2;
+    
+    System.out.println("digite um numero: ");
+    numero1 = sc.nextInt();
+    
+    System.out.println("digite um numero: ");
+    numero2 = sc.nextInt();
+    
+    System.out.println("resultado: " + (numero1 + numero2));
+    
+    
+    
+    
+    }
+}
